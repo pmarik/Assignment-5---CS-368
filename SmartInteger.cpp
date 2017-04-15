40821dbc08f1cad05b035843156e6bc94792603e
pmarik <pmarik@wisc.edu>
2017-04-15 13:40:04 -0500

    Create SmartInteger.cpp

---
+++ SmartInteger.cpp
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+// File Name:      SmartInteger.cpp
+//
+// Author:         Paul Marik, David Wissink
+// CS email:       marik@cs.wisc.edu, wissink@cs.wisc.edu
+//
+// Description:    The implementation file for the SmartInteger class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "SmartInteger.hpp"
+
+    /**
+     * @brief Default constructor for Smartinteger.
+     *
+     */
+    SmartInteger::SmartInteger(){
+        num = 0;
+    }
+
+/**
+     * @brief A parameterised constructor for a Student.
+     *
+     * @param num Int value.
+     */
+    SmartInteger::SmartInteger(int num){
+        this->num = num;
+    }
+
+
+    /**
+     * @brief Getter for smartInteger's int value.
+     *
+     * @return The num of the smartInteger.
+     */
+    const int &SmartInteger::getValue(){
+        return num;
+    }
+
+
+    std::ostream& operator<<(std::ostream& os, const SmartInteger::SmartInteger& si){
+
+        return os << si.getValue();
+    }
